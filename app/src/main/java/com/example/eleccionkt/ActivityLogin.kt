1723d2e131567a9207262c6af51c17e03030ab91
Miguel Ángel Jiménez Meléndez <miguejm@outlook.es>
2019-11-16 11:30:06 -0600

    Inico de sesión implementado con Wservice

--- app/src/main/java/com/example/eleccionkt/ActivityLogin.kt
+++ app/src/main/java/com/example/eleccionkt/ActivityLogin.kt
@@ -1,10 +1,17 @@
 package com.example.eleccionkt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.example.eleccionkt.BaseDatos.BaseDatos
+import com.example.eleccionkt.BaseDatos.VolleySingleton
+import com.example.eleccionkt.BaseDatos.address
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class ActivityLogin : AppCompatActivity() {
     private lateinit var scontrol: String
@@ -12,7 +19,7 @@ class ActivityLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        supportActionBar!!.hide()
+        supportActionBar!!.hide() //desabilita el actionbar
         etncontrol.requestFocus()
         btnLogin.setOnClickListener {
             if(etncontrol.text!!.isEmpty() || etnip.text!!.isEmpty()){
@@ -23,12 +30,20 @@ class ActivityLogin : AppCompatActivity() {
                 val nip = etnip.text.toString()
                 val admin = BaseDatos(this)
                 val result = admin.Consulta("SELECT ncontrol,nip FROM usuario")
-                if (result!!.moveToFirst()){
+                if (result != null){
                     scontrol = result.getString(0)
                     snip = result.getString(1)
                     result.close()
                     admin.close()
-                }
+                    if (ncontrol == scontrol && nip == snip) {
+                        val actividad = Intent(this,MainActivity::class.java)
+                        actividad.putExtra("ncontrol",ncontrol)
+                        actividad.putExtra("nip",nip)
+                        Toast.makeText(this,"LOGIN: $ncontrol" ,Toast.LENGTH_LONG).show()
+                        startActivity(actividad)
+                    }
+                    else Toast.makeText(this,"Su número de control o nip son incorrectos",Toast.LENGTH_SHORT).show()
+                }else Toast.makeText(this,"Capa8",Toast.LENGTH_SHORT).show()
             }
         }
     }
--- app/src/main/java/com/example/eleccionkt/MainActivity.kt
+++ app/src/main/java/com/example/eleccionkt/MainActivity.kt
@@ -10,41 +10,63 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.example.eleccionkt.BaseDatos.BaseDatos
 import com.example.eleccionkt.BaseDatos.VolleySingleton
+import com.example.eleccionkt.BaseDatos.address
+import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var  scontrol: String
+    private lateinit var scontrol: String
     private lateinit var snip: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-         //getUsuarios()
-        startActivity(Intent(this,ActivityLogin::class.java))
+        val actividad = intent
+        if (actividad != null && actividad.hasExtra("ncontrol") && actividad.hasExtra("nip")) {
+            scontrol = actividad.getStringExtra("ncontrol")
+            snip = actividad.getStringExtra("nip")
+        } else {
+            val admin = BaseDatos(this)
+            val result = admin.Consulta("Select ncontrol,nip From usuario")
+            if (result!!.moveToFirst()) {
+                scontrol = result.getString(0)
+                snip = result.getString(1)
+                result.close()
+                admin.close()
+            } else {
+                getUsuarios()
+                startActivity(Intent(this, ActivityLogin::class.java))
+            }
+        }
+        Toast.makeText(this,"LOGIN: $scontrol" ,Toast.LENGTH_LONG).show()
     }
-    fun getUsuarios(v:View){
-        Toast.makeText(this,"a",Toast.LENGTH_SHORT).show()
-        val wsURL = "http://172.16.251.219/Wservice/mostraralumnos.php"
+    fun getUsuarios() {
+        val wsURL = address.IP + "WService/MostrarAlumnos.php"
         val admin = BaseDatos(this)
         admin.Ejecuta("DELETE FROM usuario")
         val jsonObjectRequest = JsonObjectRequest(
-            Request.Method.POST,wsURL,null, Response.Listener { response ->
+            Request.Method.POST, wsURL, null, Response.Listener { response ->
                 val succ = response["success"]
                 val msg = response["message"]
-                val usuariosJson = response.getJSONArray("usuario")
-                for (i in 0 until usuariosJson.length()){
+                val usuariosJson = response.getJSONArray("usuario")//name usuario (webservice)
+                for (i in 0 until usuariosJson.length()) {
                     val ncontrol = usuariosJson.getJSONObject(i).getString("ncontrol")
                     val nombre = usuariosJson.getJSONObject(i).getString("nombre_alumno")
                     val sexo = usuariosJson.getJSONObject(i).getString("sexo")
                     val id_carrera = usuariosJson.getJSONObject(i).getString("id_carrera")
                     val nip = usuariosJson.getJSONObject(i).getString("nip")
-                    val sentencia = "INSERT INTO usuario(ncontrol,nombre_alumno,sexo,id_carrera,nip) Values('$ncontrol','$nombre','$sexo',$id_carrera,'$nip')"
+                    val sentencia =
+                        "INSERT INTO usuario(ncontrol,nombre_alumno,sexo,id_carrera,nip) Values('$ncontrol','$nombre','$sexo',$id_carrera,'$nip')"
                     var result = admin.Ejecuta(sentencia)
-                    Toast.makeText(this,"Información cargada" + result, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Información cargada: " + result, Toast.LENGTH_SHORT)
+                        .show()
                 }
             },
             Response.ErrorListener { error ->
-                Toast.makeText(this,"Error capa8: ${error.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Error capa8: ${error.message}", Toast.LENGTH_SHORT).show()
             }
         )
         VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
     }
+
+
 }
